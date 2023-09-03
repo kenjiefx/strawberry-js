34d74fd465145947399aab3393f9316d0cf43836
kenjiefx <terradoflex@gmail.com>
2023-09-03 20:15:54 +0800

    cleanup

--- src/services/blocks.js
+++
@@ -1,7 +0,0 @@
-$blocks(renderObj,renderElement){
-    // Finds all elements with Block attributes
-    let allBlockElements = strawberry.$getElementsFrom(renderElement,BLOCK_ELEMENT_ATTR);
-    for (var i = 0; i < allBlockElements.length; i++) {
-        this.$scope(renderObj,allBlockElements[i]);
-    }
-}
