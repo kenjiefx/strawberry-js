d77c2556b4fc330e380da61907e0a15adfc5c017
Kenjie Terrado <terradoflex@gmail.com>
2024-03-11 15:35:49 +0800

    scratch everything

--- src/models/dom.implementation.wrapper.ts
+++
@@ -1,6 +0,0 @@
-export class DomImplementationWrapper {
-    implementation: Document
-    constructor(name:string){
-        this.implementation = document.implementation.createHTMLDocument(name)
-    }
-}
--- src/models/scope.ts
+++
@@ -1,1 +0,0 @@
-export type ScopeObject = {[key:string]:any}
